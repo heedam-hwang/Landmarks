8da7a31fb31b48a7075a6df3013c6f360b177504
heedam-hwang <heedam21@gmail.com>
2022-02-08 03:16:57 +0900

    feat: 이미지 카테고리 뷰 추가, 프로필 뷰 및 프로필 수정 뷰 추가

--- Landmarks/Model/Landmark.swift
+++ Landmarks/Model/Landmark.swift
@@ -16,7 +16,15 @@ struct Landmark: Hashable, Codable, Identifiable {
     var state: String
     var description: String
     var isFavorite: Bool
-
+    var isFeatured: Bool
+    
+    var category: Category
+    enum Category: String, CaseIterable, Codable {
+        case lakes = "Lakes"
+        case rivers = "Rivers"
+        case mountains = "Mountains"
+    }
+    
     private var imageName: String
     
     var image: Image {
@@ -29,7 +37,7 @@ struct Landmark: Hashable, Codable, Identifiable {
             latitude: coordinates.latitude,
             longitude: coordinates.longitude)
     }
-
+    
     struct Coordinates: Hashable, Codable {
         var latitude: Double
         var longitude: Double
